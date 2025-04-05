a77d69f6f72a5c71e00da5d72b3726a120dbeedb
Kiran-4124 <kiransjcet2024@gmail.com>
2025-04-05 16:08:21 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function clicksumbitbutton(event){
+    event.preventDefault();
+    const username = document.getElementById("username").value;
+    const welcomemessage =document.getElementById('welcome-message');
+    welcomemessage.textContent ="welcome,"+username+"!";
+    welcomemessage.style.display='block';
+
+}
